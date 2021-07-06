2b55b8902ececf670eace644dbd4d4531bebc7cf
levutrunghieu0 <85774532+levutrunghieu0@users.noreply.github.com>
2021-07-06 22:13:24 +0700

    ok

--- wp-login.php
+++ wp-login.php
@@ -96,7 +96,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/myproject\/wordpress\/wp-admin\/admin-aj
 <script type="text/javascript" src="https://levutrunghieu0.github.io/mywordpress/wp-includes/js/wp-util.min.js?ver=5.7.2" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"ea1b5999db"};
+var userProfileL10n = {"user_id":"0","nonce":"4aa2ca52f1"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
